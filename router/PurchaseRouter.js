5fdb16165099547bc57187b0a95a774fa95f23fb
HariniVaradharasu <harinivaradharasu@gmail.com>
2025-01-02 14:59:31 +0530

    Add files via upload

---
+++ router/PurchaseRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { createItem, getItem, getItems, updateItem, updateItems, deleteItem, deleteItems} = require('../controller/PurchaseController');
+
+router.post("/createItem", createItem);
+router.get("/getItem/:id", getItem);
+router.get("/getItems", getItems);
+router.put("/updateItem/:id", updateItem);  
+router.put("/updateItems/:uname", updateItems);  
+router.delete("/deleteItem/:id", deleteItem);
+router.delete("/deleteItems", deleteItems);
+module.exports = router;
